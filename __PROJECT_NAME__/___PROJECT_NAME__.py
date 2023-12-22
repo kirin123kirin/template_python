578ad40a207633affd53dff68159636a7ee0862d
kirin123 <kirin123kirin@gmail.com>
2023-12-23 00:29:48 +0900

    first commit

---
+++ __PROJECT_NAME__/___PROJECT_NAME__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main():
+    pass
+
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
+    # main()
---
+++ __PROJECT_NAME__/__init__.py
@@ -0,0 +1,1 @@
+from .___PROJECT_NAME__ import __PROJECT_NAME__  # noqa: F401
